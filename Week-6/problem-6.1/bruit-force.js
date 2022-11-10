582bfc17bcc3782921b36fc08ab72099f97ad723
Deven Sitapara <shriramsoft@gmail.com>
2022-11-10 23:40:32 +0530

    exercise 6.1 done

---
+++ Week-6/problem-6.1/bruit-force.js
@@ -0,0 +1,28 @@
+const A = [1, 2, 3, 4, -10];// 10
+//const A = [-2, 1, -3, 4, -1, 2, 1, -5, 4]; // 6
+let maxSum = 0;
+let outputArray = [];
+for (let k = 0; k < A.length; k++) {
+
+  for (let i = k; i < A.length; i++) {
+    let sum = 0;
+    let array = [];
+
+    for (let j = k; j <= i; j++) {
+      sum += A[j];
+      array.push(A[j]);
+    }
+    console.log("------------");
+    console.log(sum);
+    console.log(array);
+    console.log("------------");
+    if (sum > maxSum) {
+      maxSum = sum;
+      outputArray = array;
+    }
+  }
+}
+
+console.log(maxSum);
+console.log(outputArray);
+// Time Complexity : O(N3)
