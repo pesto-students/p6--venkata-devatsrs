af5793e0bbb01cc98c00f6a1c609bbb1a0ea4f63
Deven Sitapara <shriramsoft@gmail.com>
2022-11-15 17:11:44 +0530

    minor moduler design added

--- Week-6/problem-6.1/bruit-force.js
+++ Week-6/problem-6.1/bruit-force.js
@@ -1,29 +1,48 @@
-const A = [1, 2, 3, 4, -10];// 10
-//const A = [-2, 1, -3, 4, -1, 2, 1, -5, 4]; // 6
-let maxSum = 0;
-let outputArray = [];
-for (let k = 0; k < A.length; k++) {
+/**
+ * Find Max Sum Contiguous Subarray
+ * For example: Input 1: A = [1, 2, 3, 4, -10]
+ * Output 1: 10
+ * Input 2: A = [-2, 1, -3, 4, -1, 2, 1, -5, 4] 
+ * Output 2: 6
+ */
 
-  for (let i = k; i < A.length; i++) {
-    let sum = 0;
-    let array = [];
 
-    for (let j = k; j <= i; j++) {
-      sum += A[j];
-      array.push(A[j]);
+//Facade Pattern
+const contiguousSubArray = {
+
+  findMax: function(_array){
+    this.maxSum = 0;
+    this.outputArray = [];
+    for (let k = 0; k < _array.length; k++) {
+    
+      for (let i = k; i < _array.length; i++) {
+        let sum = 0;
+        let array = [];
+    
+        for (let j = k; j <= i; j++) {
+          sum += _array[j];
+          array.push(_array[j]);
+        }
+        if (sum > this.maxSum) {
+          this.maxSum = sum;
+          this.outputArray = array;
+        }
+      }
     }
-    console.log("------------");
-    console.log(sum);
-    console.log(array);
-    console.log("------------");
-    if (sum > maxSum) {
-      maxSum = sum;
-      outputArray = array;
-    }
+
   }
-}
+};
 
-console.log(maxSum);
-console.log(outputArray);
+// Example 1 
+contiguousSubArray.findMax([1, 2, 3, 4, -10]);
+console.log(contiguousSubArray.outputArray ,   contiguousSubArray.maxSum); // 10
+
+
+// Example 2 
+contiguousSubArray.findMax([-2, 1, -3, 4, -1, 2, 1, -5, 4]); 
+console.log(contiguousSubArray.outputArray, contiguousSubArray.maxSum);//  
+
+
+
 // Time Complexity : O(N3)
 // Space Complexity : O(N2)
